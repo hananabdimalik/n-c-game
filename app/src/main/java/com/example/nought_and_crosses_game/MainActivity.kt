dbe6dc2fb8aab3799d4c66cb54de59df4e0c1667
Hanan Abdimalik <hanan.abdimalik@autotrader.co.uk>
2025-06-01 15:36:33 +0100

    first commit

---
+++ app/src/main/java/com/example/nought_and_crosses_game/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.nought_and_crosses_game
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.nought_and_crosses_game.ui.theme.NoughtandcrossesgameTheme
+import com.example.nought_and_crosses_game.ui.theme.ui.GameGrid
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NoughtandcrossesgameTheme {
+                GameGrid()
+            }
+        }
+    }
+}
+
